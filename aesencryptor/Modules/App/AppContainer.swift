69560a7277828ebcae27cbbf3b78fa3ee62d4b25
just-software-dev <idontgiveafuck@hotmail.com>
2024-03-04 16:56:13 -0300

    App Assembly

--- aesencryptor/Modules/App/AppContainer.swift
+++ aesencryptor/Modules/App/AppContainer.swift
@@ -8,5 +8,13 @@
 import Swinject
 
 struct AppContainer {
-    let assembler = Assembler([SharedAssembly()])
+    let assembler = Assembler([AppAssembly()])
+}
+
+struct AppAssembly: Assembly {
+    func assemble(container: Swinject.Container) {
+        container.register(EncryptionService.self) { _ in
+            EncryptionService()
+        }.inObjectScope(.transient)
+    }
 }
